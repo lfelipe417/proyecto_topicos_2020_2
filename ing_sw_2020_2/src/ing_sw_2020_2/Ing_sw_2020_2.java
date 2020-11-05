b9b59d56e224b37b5c979d81dc953ad98a7b34cc
luis1 <luis1@DESKTOP-AJ5I6CK>
2020-11-05 10:31:36 -0500

    Version 1.0 Integracion De NetBeans Con Github

---
+++ ing_sw_2020_2/src/ing_sw_2020_2/Ing_sw_2020_2.java
@@ -0,0 +1,25 @@
+
+package ing_sw_2020_2;
+
+/**
+ *
+ * @author luis1
+ */
+public class Ing_sw_2020_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Bienvenidos");
+        System.out.print("Integrando NetBeans con Github");
+        System.out.print("Linea adiccionada desde Github");
+        System.out.print("Hola");
+        System.out.print("Inicio rama Felipe Quiñones");
+        System.out.print("version WMRC");
+        System.out.print("VERSION 1.1");
+        System.out.print("Actualizar");
+        
+    }
+    
+}
